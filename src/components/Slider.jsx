407cdce65146af759897443c7b89eb86a8355de4
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2022-11-03 19:43:40 +0100

    .

---
+++ src/components/Slider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const Slider = () => {
+  return <Container></Container>;
+};
+
+export default Slider;
