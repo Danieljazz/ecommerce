1840f49c3a479dc21c76647c6427390cda308594
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2022-11-15 23:17:22 +0100

    Preparing api models

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = express();
 dotenv.config()
-mongoose.connect(process.env.v3n4rSH9uXW).then(() => console.log('Connection success')).catch((err) => console.log('There is error', err))
+mongoose.connect(process.env.v3n4rSH9uXW).then(() => console.log('DB Connection success')).catch((err) => console.log('There is error with DB connection', err))
 
-app.listen('3000', () => (console.log('Server is running')))
+app.get('/api', (res, req) =>{
+    res = 'dwad'
+})
+
+app.listen('5000', () => (console.log('Server is running')))
