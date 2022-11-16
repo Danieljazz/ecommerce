3df893210124cd95d4b50731d550c5b66dd1a747
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2022-11-16 22:06:52 +0100

    Added routes

--- api/index.js
+++ api/index.js
@@ -5,8 +5,14 @@ const app = express();
 dotenv.config()
 mongoose.connect(process.env.v3n4rSH9uXW).then(() => console.log('DB Connection success')).catch((err) => console.log('There is error with DB connection', err))
 
-app.get('/api', (res, req) =>{
-    res = 'dwad'
-})
+dotenv.config();
+mongoose
+  .connect(process.env.v3n4rSH9uXW)
+  .then(() => console.log("DB Connection success"))
+  .catch((err) => console.log("There is error with DB connection", err));
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.listen("5000", () => console.log("Server is running"));
 
 app.listen('5000', () => (console.log('Server is running')))
