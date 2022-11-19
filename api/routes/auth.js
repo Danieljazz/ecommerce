eed792110e686e4e85c0c90e84211d8b5b2dfe31
Daniel Jazdzewski <daniel.jazdzewski2@gmail.com>
2022-11-19 20:03:50 +0100

    Only jwt need to be added to auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,19 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const cryptoJS = require("crypto-js");
+const dotenv = require("dotenv");
+dotenv.config();
 
+// REGISTER
 router.post("/register", (req, res) => {
   const newUser = new User({
     username: req.body.username,
     mail: req.body.mail,
-    password: req.body.password,
+    password: cryptoJS.AES.encrypt(req.body.password, process.env.YWQ39Usa),
   });
   newUser
     .save()
-    .then((value) => res.status(200).json(value))
+    .then((value) => res.status(201).json(value))
     .catch((error) => res.status(500).json(error));
   // try {
   //   const user = await newUser.save();
@@ -19,4 +23,25 @@ router.post("/register", (req, res) => {
   // }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    // this solution instead of && due to header sent problem
+    if (!user) {
+      return res.status(404).json("Wrong credentials");
+    }
+    const hashedPassword = cryptoJS.AES.decrypt(
+      user.password,
+      process.env.YWQ39Usa,
+    );
+    const OriginalPassword = hashedPassword.toString(cryptoJS.enc.Utf8);
+    if (OriginalPassword !== req.body.password)
+      return res.status(401).json("Wrong credentials");
+    const { password, ...others } = user._doc; // prevent password send, getting only document so doc
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
